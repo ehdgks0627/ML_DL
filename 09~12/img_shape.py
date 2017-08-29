e4130acea39df7dcfa688e7c8a8cde9caec73afe
ehdgks0627 <ehdgks0627@naver.com>
2017-08-29 13:43:13 +0900

    add lab 11

---
+++ 09~12/img_shape.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+image = np.array([[[[1], [2], [3]],
+                   [[4], [5], [6]],
+                   [[7], [8], [9]]]], dtype=np.float32)
+print(image.shape)
+plt.imshow(image.reshape(3, 3), cmap='Greys')
+
+print("image.shape", image.shape)
+weight = tf.constant([[[[1.]], [[1.]]],
+                      [[[1.]], [[1.]]]])
+print("weight.shape", weight.shape)
+conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='VALID')
+conv2d_img = conv2d.eval()
+print("conv2d_img.shape", conv2d_img.shape)
+conv2d_img = np.swapaxes(conv2d_img, 0, 3)
+for i, one_img in enumerate(conv2d_img):
+    print(one_img.reshape(2, 2))
+    plt.subplot(1, 2, i + 1), plt.imshow(one_img.reshape(2, 2), cmap='gray')
+
+
+print("\n\nimage.shape", image.shape)
+weight = tf.constant([[[[1.]],[[1.]]],
+                      [[[1.]],[[1.]]]])
+print("weight.shape", weight.shape)
+conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='SAME')
+conv2d_img = conv2d.eval()
+print("conv2d_img.shape", conv2d_img.shape)
+conv2d_img = np.swapaxes(conv2d_img, 0, 3)
+for i, one_img in enumerate(conv2d_img):
+    print(one_img.reshape(3,3))
+    plt.subplot(1,2,i+1), plt.imshow(one_img.reshape(3,3), cmap='gray')
